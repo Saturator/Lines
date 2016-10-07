0a4191e5b9f2fab7efc2b566dd3582cb59d99d1f
Matias <matias.enkovaara@gmail.com>
2016-10-07 22:08:05 +0300

    pit ja checkstyle

--- linesengine/src/test/java/com/linesengine/math/BoxBodyTest.java
+++ linesengine/src/test/java/com/linesengine/math/BoxBodyTest.java
@@ -11,14 +11,6 @@ public class BoxBodyTest
         BoxBody b1 = new BoxBody(2f);
         b1.move(new Vector2(-2f, 0f));
         //System.out.println(b1);
-        assertEquals(b1.tl.x, -3f, 0.01f);
-        assertEquals(b1.tl.y, 1f, 0.01f);
-        assertEquals(b1.tr.x, -1f, 0.01f);
-        assertEquals(b1.tr.y, 1f, 0.01f);
-        assertEquals(b1.br.x, -1f, 0.01f);
-        assertEquals(b1.br.y, -1f, 0.01f);
-        assertEquals(b1.bl.x, -3f, 0.01f);
-        assertEquals(b1.bl.y, -1f, 0.01f);
         assertEquals(b1.position.x, b1.middle().x, 0.01f);
         assertEquals(b1.position.y, b1.middle().y, 0.01f);
     }
@@ -30,14 +22,6 @@ public class BoxBodyTest
         System.out.println(b1);
         b1.move(new Vector2(1f, 1f));
         System.out.println(b1);
-        assertEquals(b1.tl.x, 0f, 0.01f);
-        assertEquals(b1.tl.y, 2f, 0.01f);
-        assertEquals(b1.tr.x, 2f, 0.01f);
-        assertEquals(b1.tr.y, 2f, 0.01f);
-        assertEquals(b1.br.x, 2f, 0.01f);
-        assertEquals(b1.br.y, 0f, 0.01f);
-        assertEquals(b1.bl.x, 0f, 0.01f);
-        assertEquals(b1.bl.y, 0f, 0.01f);
         assertEquals(b1.position.x, b1.middle().x, 0.01f);
         assertEquals(b1.position.y, b1.middle().y, 0.01f);
     }
